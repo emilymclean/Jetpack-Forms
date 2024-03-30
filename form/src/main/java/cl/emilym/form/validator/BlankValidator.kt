6db8842920a06fd056f094abd1e229862cab435c
Emily McLean <me@emilym.cl>
2024-03-30 12:11:28 +1100

    Change blank validator default message

--- form/src/main/java/cl/emilym/form/validator/BlankValidator.kt
+++ form/src/main/java/cl/emilym/form/validator/BlankValidator.kt
@@ -4,7 +4,7 @@ import cl.emilym.form.ValidationResult
 import cl.emilym.form.Validator
 
 class BlankValidator(
-    private val message: String = "Field cannot be blank"
+    private val message: String = "Field is required"
 ): Validator<String> {
 
     override fun validate(value: String?): ValidationResult {
