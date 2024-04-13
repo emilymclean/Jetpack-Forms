4308f0a803800697f25a38bd025fb67d225e3a8a
Emily McLean <me@emilym.cl>
2024-04-13 21:27:10 +1000

    Add javadocs for extractors

--- form/src/main/java/cl/emilym/form/FormExtractor.kt
+++ form/src/main/java/cl/emilym/form/FormExtractor.kt
@@ -1,7 +1,18 @@
 package cl.emilym.form
 
+/**
+ * An interface for extracting form data into objects of type T.
+ *
+ * @param T The type of object to extract form data into.
+ */
 interface FormExtractor<T> {
 
+    /**
+     * Extracts form data into an object of type T.
+     *
+     * @param fields The list of form fields containing data to be extracted.
+     * @return An object of type T constructed using the extracted form data.
+     */
     fun extract(fields: List<FormField<*>>): T
 
 }
